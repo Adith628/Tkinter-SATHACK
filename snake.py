2c9d58b68f255af8f4c9ef88603e2dddeb03bfa3
Adithyan T <93542630+Adith628@users.noreply.github.com>
2022-12-03 22:12:01 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,56 +1,150 @@
-  # import the necessary packages
-    from tkinter import *
-    from PIL import Image
-    from PIL import ImageTk
-    from tkinter import filedialog as tkFileDialog
-    import cv2
-    def select_image():
-        # grab a reference to the image panels
-        global panelA, panelB
-        # open a file chooser dialog and allow the user to select an input
-        # image
-        path = tkFileDialog.askopenfilename()
-        # ensure a file path was selected
-        if len(path) > 0:
-            # load the image from disk, convert it to grayscale, and detect
-            # edges in it
-            image = cv2.imread(path)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            edged = cv2.Canny(gray, 50, 100)
-            # OpenCV represents images in BGR order; however PIL represents
-            # images in RGB order, so we need to swap the channels
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            # convert the images to PIL format...
-            image = Image.fromarray(image)
-            edged = Image.fromarray(edged)
-            # ...and then to ImageTk format
-            image = ImageTk.PhotoImage(image)
-            edged = ImageTk.PhotoImage(edged)
-                # if the panels are None, initialize them
-            if panelA is None or panelB is None:
-                # the first panel will store our original image
-                panelA = Label(image=image)
-                panelA.image = image
-                panelA.pack(side="left", padx=10, pady=10)
-                # while the second panel will store the edge map
-                panelB = Label(image=edged)
-                panelB.image = edged
-                panelB.pack(side="right", padx=10, pady=10)
-            # otherwise, update the image panels
-            else:
-                # update the pannels
-                panelA.configure(image=image)
-                panelB.configure(image=edged)
-                panelA.image = image
-                panelB.image = edged
-            # initialize the window toolkit along with the two image panels
-    root = Tk()
-    panelA = None
-    panelB = None
-    # create a button, then when pressed, will trigger a file chooser
-    # dialog and allow the user to select an input image; then add the
-    # button the GUI
-    btn = Button(root, text="Select an image", command=select_image)
-    btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
-    # kick off the GUI
-    root.mainloop()
+
+from tkinter import *
+import random
+
+from numpy import delete
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 100
+SPACE_SIZE = 25
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"  
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+class Snake:
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+        
+        for i in range(0,BODY_PARTS):
+            self.coordinates.append([0,0])
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x,y,x + SPACE_SIZE,y + SPACE_SIZE, fill= SNAKE_COLOR,tag="snake")
+            self.squares.append(square)
+        
+            
+class Food:
+    
+    def __init__(self):
+        x=random.randint(0,(GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y=random.randint(0,(GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
+        self.coordinates = [x,y]
+        
+        canvas.create_oval(x,y,x + SPACE_SIZE,y + SPACE_SIZE,fill=FOOD_COLOR,tag="food")
+        
+
+def next_turn(snake,food):
+    
+    x,y= snake.coordinates[0]
+    
+    if direction =="up":
+        y -= SPACE_SIZE
+    elif direction =="down":
+        y += SPACE_SIZE
+    elif direction =="left":
+        x -= SPACE_SIZE
+    elif direction =="right":
+        x += SPACE_SIZE
+    
+    snake.coordinates.insert(0,(x,y))
+        
+    square = canvas.create_rectangle(x,y,x+SPACE_SIZE,y + SPACE_SIZE,fill =  SNAKE_COLOR)
+    snake.squares.insert(0,square)
+    
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+        global score
+        score += 1
+        
+        label.config(text="Score:{}".format(score))
+        
+        canvas.delete("food")
+        
+        food = Food()
+        
+    else:
+        del snake.coordinates[-1]
+        canvas.delete(snake.squares[-1])
+        del snake.squares[-1]
+    
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(SPEED,next_turn,snake,food)  
+    
+        
+def change_direction(new_direction):
+    
+    global direction
+    if new_direction=='left':
+        if direction !='right':
+            direction = new_direction
+    elif new_direction=='right':
+        if direction !='left':
+            direction = new_direction
+    elif new_direction=='up':
+        if direction !='down':
+            direction = new_direction
+    elif new_direction=='down':
+        if direction !='up':
+            direction = new_direction
+    
+            
+
+def check_collisions(snake):
+    
+    x,y = snake.coordinates[0]
+    
+    if x < 0 or x>= GAME_WIDTH:
+        return True
+    elif y < 0 or y>= GAME_HEIGHT:
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+    return False
+    
+def game_over():
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2,canvas.winfo_height()/2,
+                       font=('consolas',70),text="GAME OVER", fill="red",tag="gameover")
+    
+window = Tk()
+window.title("Snake game")
+window.resizable(False,False)
+
+score = 0
+direction = 'down'
+
+label =Label(window, text="Score:{}".format(score),font=('consolas',40))
+label.pack()
+
+canvas = Canvas(window,bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+canvas.pack()
+
+#center the window appeared
+window.update()
+
+window_width = window.winfo_width()
+window_height= window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2)-(window_width/2))
+y = int((screen_height/2)-(window_height/2))
+
+#window.geometry(f"{window_width}*{window_height}+{x}+{y}")
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
+snake = Snake()
+food = Food()
+
+next_turn(snake,food)
+window.mainloop()
